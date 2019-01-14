0875d88fca1b70cd10cb77ff97212656489d42f9
rahmona-henry <rahmona.henry@icloud.com>
2019-01-14 13:14:34 +0000

    add tracking code

--- server.js
+++ server.js
@@ -2,5 +2,7 @@ const express = require('express')
 const app = express()
 const port= 3000
 
-app.get('/', (req, req) => res.send('Hello HubSpot'))
+app.use(express.static('client'))
+
+app.get('/', (req, res) => res.send(index.html))
 app.listen(port, () => console.log('Test App 1 listening on port ${port}!'))
